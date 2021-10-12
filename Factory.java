895fcdb2d96903e8dd9f202cbe6bc3b206ba4d26
RahafShami2 <75248980+RahafShami2@users.noreply.github.com>
2021-10-12 19:53:17 +0300

    Add files via upload

---
+++ Factory.java
@@ -0,0 +1,4 @@
+public class ProtocolFactory {
+    Connection.getInstance;
+ 
+    }    
---
+++ protocol.java
@@ -0,0 +1,9 @@
+
+package edu.najah.it.capp.asd.intf;
+
+public interface Protocol {
+	
+	public boolean release();
+	public void send(String message);
+
+}
